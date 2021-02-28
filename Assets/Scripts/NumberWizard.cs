24b45b3528d24be17195dd864ba280f9bb1d4115
Joseph Broderick <PenguinMan98@usa.net>
2021-02-28 02:16:55 -0700

    Game now works

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+
+    int guess;
+
+    // Use this for initialization
+    private void Start()
+    {
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        NextGuess();
+    }
+
+    public void onPressHigher()
+    {
+        if(guess + 1 > max)
+        {
+            min = max;
+        } else
+        {
+            min = guess + 1;
+        }
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        if(guess -1 < min)
+        {
+            max = min;
+        } else
+        {
+            max = guess - 1;
+        }
+        NextGuess();
+    }
+
+    private void NextGuess()
+    {
+        guess = UnityEngine.Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
